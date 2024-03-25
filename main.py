a3829c41438acdbac26f6a5303f1848248c8ac7d
kibzy <kibzy@fastmail.org>
2024-03-25 17:47:56 +1100

    add dictionary function for counting character

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    character_dict = count_letters(text)
     print(f"{num_words} words found in the document")
 
 def get_num_words(text):
@@ -13,4 +14,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_letters(string):
+    letter_dict = dict()
+    for letter in string:
+        lowered_letter = letter.lower()
+        if lowered_letter in letter_dict:
+            letter_dict[lowered_letter] += 1
+        else:
+            letter_dict[lowered_letter] = 1
+    # print(letter_dict)
+    return letter_dict
 main()
