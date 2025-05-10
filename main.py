347ab40cec763d8e3038b8ea02d2007939281459
kibzy <dimreaper@yepmail.net>
2025-05-10 13:31:19 +1000

    add sys for arguments

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from stats import get_num_words
+import sys
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
