bce7b47dd362b90cecf5146cda616c43b86f087b
kibzy <dimreaper@yepmail.net>
2025-05-10 13:36:57 +1000

    main uses sys arguments for book

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_dict = count_letters(text)
